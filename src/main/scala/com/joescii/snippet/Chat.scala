2646f3a8b87abc48e142eb9d33a9c66e41129553
Joe Barnes <joe@joescii.com>
2017-10-13 17:11:38 -0500

    Wired up login

---
+++ src/main/scala/com/joescii/snippet/Chat.scala
@@ -0,0 +1,39 @@
+package com.joescii.snippet
+
+import net.liftweb.http.js.JE.JsRaw
+import net.liftweb.http.js.JsCmd
+import net.liftweb.http.js.JsCmds._
+import net.liftweb.http.{S, SHtml, SessionVar}
+
+import scala.xml.NodeSeq
+import net.liftweb.util.Helpers._
+
+object User extends SessionVar[Option[String]](None)
+
+object Chat {
+  private [this] def addLoginPostHandler: Unit = for {
+    r <- S.request if r.post_?
+    name <- S.param("name")
+  } {
+    User.set(Some(name))
+  }
+
+  def render: NodeSeq => NodeSeq = {
+    addLoginPostHandler
+
+    if(User.get.isDefined) "#login [class+]" #> "hidden"
+    else "#chat [class+]" #> "hidden"
+  }
+}
+
+object LoginForm {
+  private [this] val jsToRunAfterSubmit: JsCmd = JsRaw(
+    """$('#login').addClass('hidden');
+      |$('#chat').removeClass('hidden');
+    """.stripMargin
+  )
+
+  def render =
+    "name=name" #> (SHtml.text("", name => User.set(Some(name))) ++ SHtml.hidden(() => jsToRunAfterSubmit)) andThen
+      "#email-form-2" #> SHtml.makeFormsAjax
+}
